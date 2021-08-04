50da764f482acd019061bce6a8b706652e674ee0
elouanf <83972924+elouanf@users.noreply.github.com>
2021-08-04 20:11:50 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const topLink = document.querySelector('.goToTop');
+
+topLink.addEventListener('click', () => {
+
+
+    window.scrollTo(0, 0);
+
+
+})
+
+topLink.removeEventListener('click', () => {
+
+    
+
+});
